84e6000af61dfb570ba24cd9a9ff07a64bcfe8c7
Lucas Gabriel <lucas.gabriel3.lg@gmail.com>
2025-06-15 20:04:20 -0300

    added raw FE for Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,90 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Notes";
+
 function Home() {
-    return <div>Home</div>
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note was deleted!");
+        else alert("Failed to delete");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Note created.");
+        else alert("Error while creating note");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Create a note</h2>
+        <form onSubmit={createNote}>
+          <label htmlFor="title">Title:</label>
+          <br />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+          <br />
+
+          <label htmlFor="content">Content:</label>
+          <br />
+          <input
+            type="text"
+            id="content"
+            name="content"
+            required
+            onChange={(e) => setContent(e.target.value)}
+            value={content}
+          />
+          <br />
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} key={note.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
